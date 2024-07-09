189979e8e3bd6440cfa96753eb257219061cf149
Kaan Kani İnce <kkince@newmind.ai>
2024-07-09 11:39:19 +0300

    Github-profile-finder created

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import GithubProfileFinder from "./components/github-profile-finder";
+
+function App() {
+    return (
+        <div className="App">
+            {/* Github profile finder */}
+            <GithubProfileFinder/>
+        </div>
+    );
+}
+
+export default App;
